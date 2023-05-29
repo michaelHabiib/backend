a5892ad1aa5de903bb8fad97910b7b3c46fa2bc4
Michael Ramzy <ramzymika@gmail.com>
2023-05-29 18:49:40 +0200

    third update make middleware

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ import blogRouter from './routes/blog_route';
 
 const passWord = encodeURIComponent("Michael2023#");
 const app = express()
-app.use(express.json())
 
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
+app.use(express.json())
 app.use('/api/user',userRouter)
 app.use('/api/blog',blogRouter)
 
--- controlles/blog_controller.js
+++ controlles/blog_controller.js
@@ -4,9 +4,6 @@ import User from "../modal/User";
 const ObjectId = mongoose.Types.ObjectId
 
 export const GetAllBllogs = async (req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     let Blogs 
     try {
         Blogs = await Blog.find()
