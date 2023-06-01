f3e7c8fa3269932eb589f79c380ed784f57cc0c3
Michael Ramzy <ramzymika@gmail.com>
2023-06-01 10:27:22 +0200

    update number nein delte user blog

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ const app = express()
 
 app.use(function (req, res, next) {
     //Enabling CORS
+    // headers.add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
       next();
     });
